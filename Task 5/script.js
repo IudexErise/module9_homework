b6ba801a4137de0f8657538fcb312162b6a38911
IudexErise <101091013+IudexErise@users.noreply.github.com>
2022-05-05 11:29:45 +0300

    Update script.js

--- Task 5/script.js
+++ Task 5/script.js
@@ -1,49 +1,46 @@
-function useRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onload = function() {
-      if (xhr.status != 200) {
-        console.log('Status: ', xhr.status);
-      } else {
-        const result = JSON.parse(xhr.response);
-        if (callback) {
-          callback(result);
-        }
-      }
-    };
-    
-    xhr.onerror = function() {
-      console.log('Error! Status: ', xhr.status);
-    };
-    xhr.send();
+const resultNode = document.querySelector('.result');
+const btnNode = document.querySelector('.button');
+let savedData = localStorage.getItem('savedImages');
+let numberNode = document.querySelector('.number');
+let limitNode = document.querySelector('.limit');
+
+btnNode.addEventListener('click', (event) => {    
+  event.preventDefault();
+  resultNode.textContent = "";
+   let number = numberNode.value;
+   let limit = limitNode.value;
+  if ((number < 1 || number > 10) && (limit < 1 || limit > 10)) {
+      resultNode.innerHTML = "<p>Номер страницы и лимит вне диапазона от 1 до 10</p>";
+    } else if (limit < 1 || limit > 10) {
+        resultNode.innerHTML = "<p>Лимит вне диапазона от 1 до 10</p>";
+    } else if (number < 1 || number > 10) {
+        resultNode.innerHTML = "<p>Номер страницы вне диапазона от 1 до 10</p>";
+    } else {
+        fetch(`https://picsum.photos/v2/list?page=${number}&limit=${limit}`)
+        .then((response) => {
+         return response.json();
+    })
+     .then((savedData) => {
+            localStorage.setItem('savedImages', JSON.stringify(savedData));
+            displayResult(savedData);
+        })  
+      .catch(() => {console.log('error')
+   });
   }
+});
   
-  const resultNode = document.querySelector('.result');
-  const btnNode = document.querySelector('.button');
-  
-  function displayResult(apiData) {
-    let cards = '';
-    apiData.forEach(item => {
-      const cardBlock = `
-        <div class="card">
-          <img src="${item.download_url}" class="card-image"/>
-          <p>${item.author}</p>
-        </div>`;
-      cards = cards + cardBlock;
-    });  
-    resultNode.innerHTML = cards;
-  }
-  
-  const inputValue = document.querySelector('input');
-  btnNode.addEventListener('click', () => {
-      const value = +inputValue.value;
-      if (Number.isInteger(value)) {
-          if (value < 1 || value > 10){
-              resultNode.innerHTML = "<p> Число вне диапазона от 1 до 10.</p>";
-          } else {
-              useRequest(`https://picsum.photos/v2/list/?limit=${value}`, displayResult);
-          }
-      }
-  })
-  
-    
+function displayResult(savedData) {
+    let cards = "";
+    if (savedData) {
+        savedData.forEach((item) => {
+            const cardBlock = `
+            <div class="card">
+                <img src="${item.download_url}" class="card-image">
+            </div>`;
+            cards += cardBlock; 
+        });
+        resultNode.innerHTML = cards;
+    }
+}
+
+displayResult(JSON.parse(savedData));
